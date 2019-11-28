3e894ccf4336e60bbe8c6fa41aafc95f6f2e98af
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-11-28 18:25:20 +0900

    MainActivity

--- app/src/main/java/kr/hs/emirim/lhyunsoo/armylanguage/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/lhyunsoo/armylanguage/MainActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.lhyunsoo.armylanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
+
+                // 액티비티 전환
+                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
