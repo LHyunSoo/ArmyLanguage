893b042e8361c261408b1db75e4fdfe1d4a4d655
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-11-28 18:24:53 +0900

    Splash Screen 화면

---
+++ app/src/main/java/kr/hs/emirim/lhyunsoo/armylanguage/SplashActivity.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.lhyunsoo.armylanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
+        startActivity(intent);      //메인으로 이동
+
+        finish();
+    }
+}
